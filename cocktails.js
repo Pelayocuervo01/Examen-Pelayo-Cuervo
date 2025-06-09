8f855a48d75963a4ddf101dd005c06743d7e8328
test <email@gmail.com>
2025-06-09 15:51:39 +0200

    Cocktails Almost Finished

--- cocktails.js
+++ cocktails.js
@@ -1,9 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
     const container = document.getElementById("content-container");
 
-    fetch("https://www.thecocktaildb.com/api/json/v1/1/")
-    .then(respuesta => respuesta.json())
-    .then(data => {
-        console.log(data)
-    });
+    fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink")
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            const drinks = data.drinks;
+
+            drinks.forEach(drink => {
+                const card = document.createElement("div");
+                card.classList.add("card");
+
+                const name = document.createElement("h4");
+                name.textContent = drink.strDrink;
+
+                const img = document.createElement("img");
+                img.src = drink.strDrinkThumb;
+                img.alt = drink.strDrink;
+                img.style.width = "100%";
+
+                const button = document.createElement("button");
+                button.textContent = "Add to My Order";
+                button.classList.add("save-btn");
+                button.addEventListener("click", () => {
+                    const savedDrinks = JSON.parse(localStorage.getItem("myOrder")) || [];
+
+                    const alreadyExists = savedDrinks.some(saved => saved.idDrink === drink.idDrink);
+
+                    if (!alreadyExists) {
+                        savedDrinks.push(drink);
+                        localStorage.setItem("myOrder", JSON.stringify(savedDrinks));
+                        alert(`"${drink.strDrink}" guardado en My Order.`);
+                    } else {
+                        alert(`"${drink.strDrink}" ya está en My Order.`);
+                    }
+                });
+                card.appendChild(img);
+                card.appendChild(name);
+                card.appendChild(button);
+                container.appendChild(card);
+            });
+        })
+        .catch(error => {
+            console.error("Error al obtener los cócteles", error);
+            container.textContent = "No se pudieron obtener los cócteles.";
+        });
 });
